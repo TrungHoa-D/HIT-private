af9f3e07fd158114b544b60204d69a753f03ef08
TrungHoa-D <trunghoa2k4@gmail.com>
2023-10-18 19:38:01 +0700

    input

--- src/Day1/Day1.java
+++ src/Day1/Day1.java
@@ -1,6 +1,13 @@
 package Day1;
 
+import java.util.Scanner;
+
 public class Day1 {
     public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        System.out.println("Nhap n");
+        int n= sc.nextInt();
+        System.out.println("n= "+n);
+
     }
 }
